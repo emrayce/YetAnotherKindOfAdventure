c28f4e08ca74e31be4d1c36ad3d95c380704ad89
emrayce <cedric.parpet@orange.fr>
2021-04-13 20:09:01 +0200

    Add: 1: Make the camera follow the player

---
+++ Yet Another Kind of Adventure/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(target);
+    }
+
+    // Be sure to modify the position after the target moves
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
