6e45246eaef120e1b68a435a0d607d84679da2aa
emrayce <cedric.parpet@orange.fr>
2021-04-14 21:40:30 +0200

    Refactor: 3: rename HealthBar to BarScript to make it more generic

---
+++ Yet Another Kind of Adventure/Assets/Scripts/BarScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    [SerializeField]
+    public Slider slider;
+
+    private int value;
+
+    public void SetMaxValue(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetValue(int health)
+    {
+        slider.value = health;
+    }
+
+    private void Start()
+    {
+        value = 100;
+        SetMaxValue(100);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            value -= 10;
+            SetValue(value);
+        }
+    }
+}
