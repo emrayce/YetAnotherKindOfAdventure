3505f0dd52072efb068246ab3d45eb2b7e0b0483
emrayce <cedric.parpet@orange.fr>
2021-04-14 23:00:01 +0200

    Add: 3: field for hp/mana slider and method to set hp/mana

--- Yet Another Kind of Adventure/Assets/Scripts/Unit.cs
+++ Yet Another Kind of Adventure/Assets/Scripts/Unit.cs
@@ -1,21 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
     [SerializeField]
-    private int hp, mana, strength, agility, intelligence;
+    private int hpMax, hp;
+    [SerializeField]
+    private int manaMax, mana;
+    
+    [SerializeField]
+    private int strength, agility, intelligence;
+
+    [SerializeField]
+    private BarScript healthbar, manabar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthbar.SetMaxValue(hpMax);
+        manabar.SetMaxValue(manaMax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Call it when modifying current health (eg: taking damage)
+    private void SetHealth(int value)
+    {
+        hp = value;
+        healthbar.SetValue(value);
+    }
+
+    // Call it when modifying max health (eg: Lvl up)
+    private void SetMaxHealth(int value)
+    {
+        hpMax = value;
+        hp = value;
+        healthbar.SetMaxValue(value);
+    }
+
+    // Call it when modifying current mana (eg: casting a spell)
+    private void SetMana(int value)
+    {
+        hp = value;
+    }
+
+    // Call it when modifying max mana (eg: Lvl up)
+    private void SetMaxMana(int value)
+    {
+        hpMax = value;
+        hp = value;
     }
 }
