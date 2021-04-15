66c2494f1575ae1de03ffdf669a4eeb549e0e1c1
emrayce <cedric.parpet@orange.fr>
2021-04-15 19:35:42 +0200

    refactor: 4: Make PlayerController inherit unit

--- Yet Another Kind of Adventure/Assets/Scripts/PlayerController.cs
+++ Yet Another Kind of Adventure/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Unit
 {
-    public float speed;
+    [SerializeField]
+    private BarScript healthbar, manabar;
+
+
     public LayerMask layers;
     public float distanceBeforeRunning;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        healthbar.SetMaxValue(hpMax);
+        manabar.SetMaxValue(manaMax);
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         Vector3 mouse = Input.mousePosition;
         Ray castPoint = Camera.main.ScreenPointToRay(mouse);
@@ -35,5 +39,48 @@ public class PlayerController : MonoBehaviour
                 transform.position = Vector3.MoveTowards(transform.position, hit.point, step);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetMaxHealth(hpMax + 10);
+            SetMaxMana(manaMax + 10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetHealth(hp - 10);
+            SetMana(mana - 10);
+        }
+    }
+
+
+    // Call it when modifying current health (eg: taking damage)
+    protected override void SetHealth(int value)
+    {
+        hp = value;
+        healthbar.SetValue(value);
+    }
+
+    // Call it when modifying max health (eg: Lvl up)
+    protected override void SetMaxHealth(int value)
+    {
+        hpMax = value;
+        hp = value;
+        healthbar.SetMaxValue(value);
+    }
+
+    // Call it when modifying current mana (eg: casting a spell)
+    protected override void SetMana(int value)
+    {
+        mana = value;
+        manabar.SetValue(value);
+    }
+
+    // Call it when modifying max mana (eg: Lvl up)
+    protected override void SetMaxMana(int value)
+    {
+        manaMax = value;
+        mana = value;
+        manabar.SetMaxValue(value);
     }
 }
