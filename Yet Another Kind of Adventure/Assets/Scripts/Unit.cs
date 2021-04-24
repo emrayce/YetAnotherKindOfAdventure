ed24621403bbf60c43b0d6194e58eb96da5a7c9b
emrayce <cedric.parpet@orange.fr>
2021-04-24 03:53:58 +0200

    Add: 4: animator field for Unit

--- Yet Another Kind of Adventure/Assets/Scripts/Unit.cs
+++ Yet Another Kind of Adventure/Assets/Scripts/Unit.cs
@@ -7,4 +7,14 @@ public class Unit : MonoBehaviour
     // movement speed
     [SerializeField]
     protected float speed;
+
+    [SerializeField]
+    protected Animator animator;
+
+    #region getter
+    public Animator GetAnimator()
+    {
+        return animator;
+    }
+    #endregion
 }
