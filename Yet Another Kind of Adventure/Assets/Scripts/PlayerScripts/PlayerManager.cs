7d7d9bad98607f61839f63704bb100aac642b4a2
emrayce <cedric.parpet@orange.fr>
2021-04-29 01:19:56 +0200

    Refacto: 0: Rename PlayerController to PlayerManager as iot role is now to manage all player associated scripts

--- Yet Another Kind of Adventure/Assets/Scripts/PlayerScripts/PlayerManager.cs
+++ Yet Another Kind of Adventure/Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
     [SerializeField]
     private Player player;
