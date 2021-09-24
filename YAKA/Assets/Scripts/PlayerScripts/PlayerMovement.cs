18a65c77301f85bb8940cb24da230656c434e4be
emrayce <emray@LAPTOP-2KMJQ760>
2021-09-24 13:50:27 +0200

    Add: start of method to keep player to the ground

--- YAKA/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ YAKA/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -6,9 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
 
+    private RaycastHit hit;
+
     public void MoveTo(Vector3 position)
     {
         transform.LookAt(new Vector3(position.x, transform.position.y, position.z));
         transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+    }
+
+    public bool IsGrounded()
+    {
+        Physics.Raycast(transform.position, Vector3.down, out hit);
+
+        return hit.distance < 0.1f;
+    }
+
+    public void KeepGrounded()
+    {
+        if (IsGrounded())
+        {
+            
+        }
     }
 }
